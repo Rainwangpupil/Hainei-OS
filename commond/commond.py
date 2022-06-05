b9441787db8c5c0a17cf8989384094fe8c5dfe09
rainwangpupil <rain-19121186325@outlook.com>
2022-06-05 15:42:50 +0800

    增加了新的命令，改进了性能

--- commond/commond.py
+++ commond/commond.py
@@ -1,8 +1,4 @@
-# coding=utf-8
-from calendar import c
 from os import system
-import cmd
-import sys
 print('欢迎来到海内操作系统，版本0.0.1')
 print("Hello, if you are not a Chinese and want to use the Hainei OS, please do not denigrate China (if you denigrate China). China's sacred dignity cannot be violated by you, and once you slander China, the 1.4 billion people in our country (China) will not forgive you.")
 print('もしあなたが日本人なら、前任者が犯した凶悪犯罪を認めて下さい、南京大虐殺で、少なくとも30万人が殺されました、あなたの指導者によって"洗脳"されないでください、私は、あなたが日本を愛することを許します、しかし、あなたは、あなたの前任者が中国で犯したtを認めなくてはいけません')
@@ -14,13 +10,17 @@ dict_commond = {'创建文件夹':'mkdir',
                 "下载软件":"sudo apt install",
                 "更新所有软件":"sudo apt upgrade",
                 "开源系统官网":"start https://github.com/Rainwangpupil/Hainei-OS",
-                '查看现在目录':'pwd'
+                '查看现在目录':'pwd',
+                '打开网站':'start',
+                'dos黑客工具':'start Software/dos_attack.py',
+                '抽人器':'start Software/随机抽人器.py'
                 }
 
+
 while True:
-    commond = input('海内系统>').split(' ')
-    commond[0] = dict_commond[commond[0]]  
+    commond = input('海内系统>').split(' ') # 分解命令与命令值
+    commond[0] = dict_commond[commond[0]]  # 把海内系统命令转换为Linux系统命令
     if len(commond) < 2:
-      commond.append('')
-    commond_out = commond[0]+ ' ' + commond[1]
-    system(commond_out)
+      commond.append('') # 将一些没有命令值的命令填充命令值''
+    commond_out = commond[0]+ ' ' + commond[1] # 分解后，将它们合并，作为Linux命令
+    system(commond_out) # 运行命令
